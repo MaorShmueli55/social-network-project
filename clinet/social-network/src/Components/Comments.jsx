602b67b4b6bc39e3b1aa5c7eaebe203fc37bd0fb
MaorShmueli55 <maorshmueli5@gmail.com>
2024-12-07 18:11:20 +0200

    11

--- clinet/social-network/src/Components/Comments.jsx
+++ clinet/social-network/src/Components/Comments.jsx
@@ -1,31 +1,43 @@
 import { useSelector } from "react-redux";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import { deleteComment } from "../utils/commentApi.js";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
 
 const Comments = ({ commentsData }) => {
   const user = useSelector((state) => state.user);
 
   return (
     <>
-      <div className="max-w-2xl mx-auto px-4 py-4">
+      <div className="max-w-2xl px-4 py-7">
         {commentsData.map((commentData) => (
           <div
             key={commentData._id}
             className="flex gap-4 mb-6 border-b border-bgBtnColor-200 pb-3"
           >
-            {/* <div className="w-12 h-12 rounded-full bg-gray-300 flex-shrink-0">
-              <img
-                src={commentData.userProfile || "/default-avatar.png"}
-                alt={`${commentData.username}'s profile`}
-                className="w-full h-full rounded-full object-cover"
-              />
-            </div> */}
+            <div className="w-12 h-12 ">
+              {commentData.profileImg ? (
+                <img
+                  src={commentData.profileImg}
+                  className="w-full h-full rounded-full object-cover"
+                />
+              ) : (
+                <Person2OutlinedIcon
+                  sx={{
+                    border: "solid 2px gray",
+                    borderRadius: "50%",
+                    padding: "2px",
+                    width: "100%",
+                    height: "100%",
+                  }}
+                />
+              )}
+            </div>
             <div className="flex-1">
-              <div className="text-sm">
-                {commentData.CommentText}
-                <span className="font-semibold ml-2">
+              <div className="text-sm flex flex-col gap-0.5">
+                <span className="mr-2 font-bold text-red-300">
                   {commentData.username}
                 </span>
+                {commentData.CommentText}
               </div>
               <div className="text-xs text-gray-500 mt-1">
                 {new Date(commentData.createdAt).toLocaleString()}
